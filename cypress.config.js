adce08c51e21e68f68387770d6dbc41c2c619b53
George Lukoff <ylukoven@gmail.com>
2023-02-27 14:44:24 -0800

    Still WIP

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
     baseUrl: "https://sales.crowdstreet.com/",
     watchForFileChanges: false,
     chromeWebSecurity: false,
     viewportWidth: 1000,
-    viewportHeight: 1600
+    viewportHeight: 1600,
   },
 });
